a303f1049e5d6babdb590996f4279c5ed262e300
tomas-jeong543 <wjdaudgns03@naver.com>
2025-05-30 23:46:20 +0900

    Update Reverse.cpp

--- Reverse.cpp
+++ Reverse.cpp
@@ -1,6 +1,12 @@
 ﻿#include "Reverse.h"
 #include<iostream>
 
+Reverse::Reverse()
+{
+    //난수를 발생 시킴
+    srand(unsigned(time(0)));
+}
+
 void Reverse::init() {
     for (int i = 0; i < 20; i++)
         for (int j = 0; j < 14; j++)
@@ -10,12 +16,6 @@ void Reverse::init() {
 
 void Reverse::block_start(Block* b) {
     b->start_Reversed();
-}
-
-bool Reverse::isCleared()
-{
-    if (stages->get_clear_line(level) <= lines) return true;
-    else return false;
 }
 
 //충돌이 일어났을 때 병합되는 과정에서 블록 중 일부가 게임판 바깥을 벗어났는 지 확인 이 프로그램에서는 이 함수 호출 시에는 늘 충돌이 발생했을 때 호출된다는 점 아시고 보면 이해가 더 빠릅니다
@@ -38,31 +38,6 @@ bool Reverse::check_overflow()
 	//아니면 false반환
 	return false;
 }
-
-/*int Reverse::check_full_line()
-{
-    int i, j;
-    for (i = 0; i < 4; i++)
-    {
-        for (j = 0; j < 4; j++)
-        {
-            total_block[curr_block->get_y() + i][curr_block->get_x() + j] |= curr_block->get_number(i, j);
-        }
-    }
-    int is_clear = check_full_line();
-    if (is_clear == 3) {
-        return 3;
-    }
-    printer->show_total_block(total_block, level);
-    if (is_clear == 4) {
-        return 4;
-    }
-    if (is_clear == 5) {
-        return 5;
-    }
-
-    return 0;
-}*/
 
 int Reverse::check_full_line()
 {
@@ -119,13 +94,13 @@ int Reverse::check_full_line()
                 printf("  ");
                 Sleep(10);
             }
-
-            //바뀐 부분 기존의 코드와 완전히 반대로 바꿨습니다 위의 블록을 지우고 아래 블록을 위의 블록으로 한 칸식 올리는
-             //방식으로 수정했습니다
+            
+           //바뀐 부분 기존의 코드와 완전히 반대로 바꿨습니다 위의 블록을 지우고 아래 블록을 위의 블록으로 한 칸식 올리는
+            //방식으로 수정했습니다
             for (k = i; k < 20; k++)
             {
                 for (j = 1; j < 13; j++)
-                    total_block[k][j] = total_block[k + 1][j];
+                    total_block[k ][j] = total_block[k + 1 ][j];
             }
             for (j = 1; j < 13; j++)
                 total_block[20][j] = 0;
@@ -137,76 +112,14 @@ int Reverse::check_full_line()
         }
 
     }
-
-
     return 0;
-}
-
-void Reverse::draw_ghostBlock()
-{
-    if (prev_ghostBlock != nullptr) {
-        printer->erase_ghostBlock(*prev_ghostBlock);
-        delete prev_ghostBlock;
-        prev_ghostBlock = nullptr;
-    }
-
-    Block* ghostBlock = new Block(*curr_block);
-    while (true) {
-        ghostBlock->move_up();
-        if (check_collision(ghostBlock)) {
-            ghostBlock->move_down();
-            break;
-        }
-    }
-    printer->show_ghostBlock(*ghostBlock);
-    prev_ghostBlock = ghostBlock;
-}
-
-bool Reverse::check_collision(Block* b)
-{
-    //바닥 도달 시 1반환
-    if (b->get_y() <= 0) {
-        return true;
-    }
-
-    //추가된 조건들 21이상이면 계속 내려오게 충돌 설정을 피합니다
-    if (b->get_y() >= 21) {
-
-        //조건 추가 좌우 장벽에 충돌하는 경우에 대한 예외처리를 위해 이중 for문을 이용해 현재 블록이 위치한 좌표가 좌우 경계를 넘어가면
-        //1을 반환하게 바꿨습니다
-        for (int i = 0; i < 4; i++)
-            for (int j = 0; j < 4; j++) {
-                if (b->get_number(i, j) == 1) {
-                    int x = b->get_x() + j;
-                    int y = b->get_y() + i;
-                    if (x < 0 || x >= 13) {
-                        return true;
-                    }
-
-                }
-            }
-        //좌우 충돌하지 않는 경우에 대해서는 0반환
-        return false;
-    }
-
-    for (int i = 0; i < 4; i++)
-        for (int j = 0; j < 4; j++) {
-            if (b->get_number(i, j) == 1) {
-                int x = b->get_x() + j;
-                int y = b->get_y() + i;
-                if (x < 0 || x >= 13 || y < 0 || total_block[y][x] == 1) {
-                    return true;
-                }
-
-            }
-        }
-    return false;
 }
 
 int Reverse::strike_check() {
 
 	//바닥 도달 시 1반환
     if (curr_block->get_y() <= 0) {
+
         return 1;
     }
 
@@ -220,8 +133,9 @@ int Reverse::strike_check() {
 				if (curr_block->get_number(i, j) == 1) {
 					int x = curr_block->get_x() + j;
 					int y = curr_block->get_y() + i;
-					if (x < 0 || x >= 13 ) {
-						return 1;
+                    //변경된 내용 충돌의 종류를 구분함 다른 블록이랑 닫는 게 아니라 좌우 경계 충돌 시에는 2를 반환하고 그 경우에는 게임종료를 시키지 않는 방식으로 코드를 개선함
+                    if (x < 0 || x >= 13 ) {
+						return 2;
 					}
 				}
 			}
@@ -248,17 +162,26 @@ int Reverse::strike_check() {
 int Reverse::move_block() {
 	printer->erase_cur_block(*curr_block);
 
-	curr_block->move_up();	
+	curr_block->move_up();
+
+    //새로 선언된 변수 y좌표가 21이상일 때 다른 블록과 충돌이 아니라 좌우 경계와의 충돌인 경우에 대해서는 false를 가진다 나머지 경우 true값을 가진다
+    bool detOverflow = true;
+
+	if (strike_check() == 1 || strike_check() == 2)
+	{
+        //다른 블록과 충돌이 아닌 좌우 경계와의 충돌인 경우 false로 값 변환
+        if (strike_check() == 2) {
+            detOverflow = false;
+        }
 
 
-	if (strike_check() == 1)
-	{	
 		curr_block->move_down();
 		//블록이 충돌한 경우에 대해서 게임 화면을 벗어나는 지를 확인하는 함수로 게임화면을 벗어나면 true를 반환하며 이 경우 게임 클리어 실패로 판별되어 false가 반환됩니다
-		if (check_overflow()) {
-            printer->SetColor(3);
+        //추가된 내용으로 블록이 모든 블록을 나타내는 경계에 들어가기 전 좌우 충돌하는 경우에는 게임 종료를 시키지 않기 위해 strike함수의 반환값을 조정했고 이를 통해서 detOverflow값을 변경해서 게임 종료 여부를 변경하게 했습니다
+		if (check_overflow() && detOverflow) {
+            /*printer->SetColor(3);
             std::cout << curr_block->get_x() << " , " << curr_block->get_y() << std::endl;
-            system("pause");
+            system("pause");*/
             return 1;
 		}
 		if (curr_block->get_y() >= 20 )	//게임오버 조건 21이 아니라 20이어야 합니다
