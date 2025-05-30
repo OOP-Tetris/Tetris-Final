e76a0b9b3c87ed7420ff8c57e94b9f0a4831767a
tomas-jeong543 <wjdaudgns03@naver.com>
2025-05-30 20:31:25 +0900

    Update Reverse.h

--- Reverse.h
+++ Reverse.h
@@ -9,4 +9,7 @@ public:
     void block_start(Block* b) override;
     //추가된 함수로 이 함수와 move_block함수 그리고 strike_check함수 내용이 변경 및 추가되었습니다
     bool check_overflow();
+    //이 함수가 문제라 오버라이드해서 다시 재정의했습니다
+    int check_full_line() override;
+ 
 };
