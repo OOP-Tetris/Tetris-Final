138da954f2f8ef06a49346b9f2e98a79033c2162
tomas-jeong543 <wjdaudgns03@naver.com>
2025-05-30 20:30:02 +0900

    Update Game.h

--- Game.h
+++ Game.h
@@ -38,11 +38,13 @@ public:
     virtual void block_start(Block* b) = 0;
     virtual void reset_stage();
     virtual int operate_key(int keytemp);
+    //이 함수가 거꾸로 나라에는 맏지 않아 가상함수로 받고 Reverse.h에 override해서 재정의했습니다
+    virtual int check_full_line();
+    
 
     void run(); // 공통 게임 루프
     void play_loop();
     int merge_block();
-    int check_full_line();
     int rotate();
     void setLevel(int le) {
         level = le;
