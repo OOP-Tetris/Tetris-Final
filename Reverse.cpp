5628393d10721108359df6988216e634d243877a
tomas-jeong543 <wjdaudgns03@naver.com>
2025-05-30 20:30:44 +0900

    Update Reverse.cpp

--- Reverse.cpp
+++ Reverse.cpp
@@ -24,13 +24,116 @@ bool Reverse::check_overflow()
 				int x = curr_block->get_x() + j;
 				int y = curr_block->get_y() + i;
 				//충돌 발생한 블록의 일부가 게임 화면의 영역을 벗어나면 게임 종료조건을 충족하기 때문에 true를 반환합니다
-				if (y >= 20) {
+				if (y >= 21) {
 					return true;
 				}
 			}
 		}
 	//아니면 false반환
 	return false;
+}
+
+/*int Reverse::check_full_line()
+{
+    int i, j;
+    for (i = 0; i < 4; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            total_block[curr_block->get_y() + i][curr_block->get_x() + j] |= curr_block->get_number(i, j);
+        }
+    }
+    int is_clear = check_full_line();
+    if (is_clear == 3) {
+        return 3;
+    }
+    printer->show_total_block(total_block, level);
+    if (is_clear == 4) {
+        return 4;
+    }
+    if (is_clear == 5) {
+        return 5;
+    }
+
+    return 0;
+}*/
+
+int Reverse::check_full_line()
+{
+    int i, j, k;
+    for (i = 0; i < 20; i++)
+    {
+        for (j = 1; j < 13; j++)
+        {
+            if (total_block[i][j] == 0)
+                break;
+        }
+        if (j == 13)	//한줄이 다 채워졌음
+        {
+
+            lines++;
+            //줄과 레벨 확인
+
+
+
+            if (stages->get_clear_line(level) <= lines) {
+                if (level < 10) {
+                    lines = 0;
+                    level++;
+                    init();
+                    cleared = true;
+                    printer->show_gamestat(level, score, stages->get_clear_line(level) - lines);
+                    return 4;
+                }
+                //만약 레벨이 10이 되는 경우 쇼우 클리어를 진행
+                else {
+                    //show_clear_screen이라는 것에서 1의 값을 받으면 3을 반환
+                    // --> merge_block()에서 이 값에 따른 반환값을 만들도록함
+                    //메인 함수에서 is_game_over이 3일때 break함
+                    //이때 화면에서 show_logo()가 잘 보이지 않는다는 것을 확인하여 show_logo() 앞 cls를 통해 지우고 다시 그리도록 함.
+
+                    printer->show_total_block(total_block, level);
+                    lines = 0;
+                    printer->show_gamestat(level, score, stages->get_clear_line(level) - lines);
+                    if (printer->show_clear_screen(score)) return 3;
+                }
+            }
+
+            printer->show_total_block(total_block, level);
+            printer->SetColor(BLUE);
+            printer->gotoxy(1 * 2 + printer->get_x(), i + printer->get_y());
+            for (j = 1; j < 13; j++)
+            {
+                printf("□");
+                Sleep(10);
+            }
+            printer->gotoxy(1 * 2 + printer->get_x(), i + printer->get_y());
+            for (j = 1; j < 13; j++)
+            {
+                printf("  ");
+                Sleep(10);
+            }
+            
+           //바뀐 부분 기존의 코드와 완전히 반대로 바꿨습니다 위의 블록을 지우고 아래 블록을 위의 블록으로 한 칸식 올리는
+            //방식으로 수정했습니다
+            for (k = i; k < 20; k++)
+            {
+                for (j = 1; j < 13; j++)
+                    total_block[k ][j] = total_block[k + 1 ][j];
+            }
+            for (j = 1; j < 13; j++)
+                total_block[20][j] = 0;
+            score += 100 + (level * 10) + (rand() % 10);
+            printer->show_gamestat(level, score, stages->get_clear_line(level) - lines);
+            i++;
+
+            return 5;
+        }
+
+    }
+
+
+    return 0;
 }
 
 int Reverse::strike_check() {
@@ -53,7 +156,6 @@ int Reverse::strike_check() {
 					if (x < 0 || x >= 13 ) {
 						return 1;
 					}
-
 				}
 			}
 		//좌우 충돌하지 않는 경우에 대해서는 0반환
@@ -87,7 +189,10 @@ int Reverse::move_block() {
 		curr_block->move_down();
 		//블록이 충돌한 경우에 대해서 게임 화면을 벗어나는 지를 확인하는 함수로 게임화면을 벗어나면 true를 반환하며 이 경우 게임 클리어 실패로 판별되어 false가 반환됩니다
 		if (check_overflow()) {
-			return 1;
+            printer->SetColor(3);
+            std::cout << curr_block->get_x() << " , " << curr_block->get_y() << std::endl;
+            system("pause");
+            return 1;
 		}
 		if (curr_block->get_y() >= 20 )	//게임오버 조건 21이 아니라 20이어야 합니다
 		{
