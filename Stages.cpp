6df415ca6bf5f002869f97be157851091742e5fc
tomas-jeong543 <wjdaudgns03@naver.com>
2025-06-01 15:21:49 +0900

    Update Stages.cpp

--- Stages.cpp
+++ Stages.cpp
@@ -9,7 +9,7 @@ Stages::Stages() {
 	stage_data[1].clear_line=2;
 	stage_data[2].speed=35;
 	stage_data[2].stick_rate=18;
-	stage_data[2].clear_line=0;
+	stage_data[2].clear_line=2;
 	stage_data[3].speed=30;
 	stage_data[3].stick_rate=17;
 	stage_data[3].clear_line=2;
@@ -18,7 +18,7 @@ Stages::Stages() {
 	stage_data[4].clear_line=2;
 	stage_data[5].speed=20;
 	stage_data[5].stick_rate=14;
-	stage_data[5].clear_line=0;
+	stage_data[5].clear_line=2;
 	stage_data[6].speed=15;
 	stage_data[6].stick_rate=14;
 	stage_data[6].clear_line=2;
@@ -27,7 +27,7 @@ Stages::Stages() {
 	stage_data[7].clear_line=2;
 	stage_data[8].speed=6;
 	stage_data[8].stick_rate=12;
-	stage_data[8].clear_line=0;
+	stage_data[8].clear_line=2;
 	stage_data[9].speed=4;
 	stage_data[9].stick_rate=11;
 	stage_data[9].clear_line=4;
